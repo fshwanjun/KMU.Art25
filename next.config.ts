c845d477826100c7d59e136ad9eefcbda60cddab
yuhwanjun <yuhwanjun0311@gmail.com>
2025-10-10 00:45:03 +0900

    #2 배포 액션 세팅

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: { unoptimized: true },
-  trailingSlash: true,
+  output: "export", // 정적 내보내기
+  images: { unoptimized: true }, // 이미지 최적화 끔
+  basePath: "/2025_graduation_exhibition", // 하위폴더 경로
+  trailingSlash: true, // 각 페이지를 폴더형 index.html로
 };
 
 export default nextConfig;
