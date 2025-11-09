7cbc6b0936ec55904a541f667215acdec85eb535
yuhwanjun <yuhwanjun0311@gmail.com>
2025-11-09 12:57:37 +0900

    #1 image 링크 문제 해결

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,7 +3,11 @@ export default function Header() {
   return (
     <header className="py-4 px-4 fixed top-0 left-0 right-0 z-50 flex flex-row items-start">
       <Link href="/">
-        <img src="/images/home.png" alt="logo" className="w-auto max-h-32" />
+        <img
+          src={`${process.env.NEXT_PUBLIC_BASE_PATH ?? ""}/images/home.png`}
+          alt="logo"
+          className="w-auto max-h-32"
+        />
       </Link>
       <nav className="flex gap-8 font-yeoleum uppercase cursor-pointer">
         <Link href="/about">About</Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,23 @@ export default function Home() {
   return (
     <div className="fixed top-0 left-0 h-full w-full overflow-hidden">
       <NoisyBlurText
-        src="/images/main.png"
+        src={`${process.env.NEXT_PUBLIC_BASE_PATH ?? ""}/images/main.png`}
         className="absolute top-0 left-0 w-full h-full z-10"
       />
       <div className="absolute top-0 left-0 w-full h-full z-0">
         <img
           className="absolute h-3/7 -top-[15%] right-[5%] opacity-30"
-          src="/images/title-1.svg"
+          src={`${process.env.NEXT_PUBLIC_BASE_PATH ?? ""}/images/title-1.svg`}
           alt="title 1"
         />
         <img
           className="absolute h-3/7 top-1/2 -translate-y-1/2 left-[10%] opacity-30"
-          src="/images/title-2.svg"
+          src={`${process.env.NEXT_PUBLIC_BASE_PATH ?? ""}/images/title-2.svg`}
           alt="title 2"
         />
         <img
           className="absolute h-3/7 -bottom-[15%] right-[15%] opacity-30"
-          src="/images/title-3.svg"
+          src={`${process.env.NEXT_PUBLIC_BASE_PATH ?? ""}/images/title-3.svg`}
           alt="title 3"
         />
       </div>
